14e193a141aa11d1ff2227d553d2054a6758236e
Mina Shokouhi <minaashokouhi@gmail.com>
2025-05-28 00:25:30 +0330

    Feat(step0-setupProject): Setup axios configuration

---
+++ src/services/apiInterceptors/requestInterceptor.ts
@@ -0,0 +1,13 @@
+import type { InternalAxiosRequestConfig } from 'axios'
+
+export function onReqFullfilled(request: InternalAxiosRequestConfig<any>) {
+  if (request.headers) {
+    request.headers['Authorization'] = `Token ${'token'}`
+  }
+  return request
+}
+
+//(error: (error: any) => any) | null, options?: AxiosInterceptorOptions) => number
+export function onReqRejected(error: unknown | null) {
+  console.log('request error:', error)
+}
