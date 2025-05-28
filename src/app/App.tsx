75bef200ad555339df94b7db0a581541f3d80827
Mina Shokouhi <minaashokouhi@gmail.com>
2025-05-28 18:16:47 +0330

    Feat(step1-implementAuthProcess): Setup queryClient to enable using of react-query

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
-import { Login, Register } from '../pages'
+import { Home, Login, Register } from '../pages'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './config/queryClient'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
